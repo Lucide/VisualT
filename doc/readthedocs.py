48724dd874127f334033814ffa9d9cec83036deb
Nemo-LUCE <artemiscode@hotmail.it>
2021-01-28 22:54:23 +0100

    readthedocs support
    attempt 1

---
+++ doc/readthedocs.py
@@ -0,0 +1,14 @@
+# -- readthedocs CI ----------------------------------------------------------
+
+# import os
+
+# read_the_docs_build = os.environ.get('READTHEDOCS', None) == "True"
+
+# if read_the_docs_build:
+import subprocess
+import sys
+
+subprocess.check_call([sys.executable, "-m", "pip", "install", "cmake"], shell=True)
+subprocess.check_call(["cmake", "-D VisualT_ASSUME_LITTLE_ENDIAN:BOOL=YES", "-S .", "-B build"], shell=True)
+subprocess.check_call(["cmake", "--build build", "--target VisualT_doxygen_generate_xml"], shell=True)
+subprocess.call([sys.executable, "build/doc/conf.py"])
