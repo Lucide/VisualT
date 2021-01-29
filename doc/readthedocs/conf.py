7d05cffaa8b7ad5eea156a702bff29239f973e27
Nemo-LUCE <artemiscode@hotmail.it>
2021-01-29 11:45:36 +0100

    readthedocs support
    attempt 7
    #noci

--- doc/readthedocs/conf.py
+++ doc/readthedocs/conf.py
@@ -8,6 +8,6 @@
 import subprocess
 import sys
 
-subprocess.check_call(["cmake", "-D VisualT_ASSUME_LITTLE_ENDIAN:BOOL=YES", "-S .", "-B build"], cwd="../..", shell=True)
-subprocess.check_call(["cmake", "--build build", "--target VisualT_doxygen_generate_xml"], cwd="../..", shell=True)
-subprocess.call([sys.executable, "build/doc/conf.py"], cwd="../..")
+subprocess.check_call(["cmake", "-D VisualT_ASSUME_LITTLE_ENDIAN:BOOL=YES", "-S .", "-B ./build"], cwd="../..", shell=True)
+subprocess.check_call(["cmake", "--build ./build", "--target VisualT_doxygen_generate_xml"], cwd="../..", shell=True)
+subprocess.call([sys.executable, "./build/doc/conf.py"], cwd="../..")
