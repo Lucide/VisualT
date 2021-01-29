c1a7ad7d2044df0fd32f0187a1ebfa387c2d1a5d
Nemo-LUCE <artemiscode@hotmail.it>
2021-01-29 12:38:03 +0100

    readthedocs support
    correct exec call
    attempt 9
    #noci

--- doc/readthedocs/conf.py
+++ doc/readthedocs/conf.py
@@ -10,4 +10,4 @@ import sys
 
 subprocess.check_call(["cmake -D VisualT_ASSUME_LITTLE_ENDIAN:BOOL=YES -S . -B build"], cwd="../..", shell=True)
 subprocess.check_call(["cmake --build build --target VisualT_doxygen_generate_xml"], cwd="../..", shell=True)
-subprocess.call([sys.executable, "build/doc/conf.py"], cwd="../..")
+exec("../conf.py")
