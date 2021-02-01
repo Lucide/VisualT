3d41bb868d464d0bd3113e36180d3bdac6543255
Nemo-LUCE <artemiscode@hotmail.it>
2021-02-01 15:59:39 +0100

    readthedocs support
    attempt 15
    - copy workaround
    
    #no-ci

--- doc/readthedocs/conf.py
+++ doc/readthedocs/conf.py
@@ -2,7 +2,7 @@
 
 import subprocess
 
-subprocess.run(["cp -r -l -n doc/!(readthedocs) doc/readthedocs"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd="../..", shell=True, check=True)
+subprocess.run(["cp", "-r", "-l", "-n", "doc/!(readthedocs)", "doc/readthedocs"], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd="../..", shell=True, check=True)
 subprocess.run(["cmake -D VisualT_ASSUME_LITTLE_ENDIAN:BOOL=YES -S . -B build"], cwd="../..", shell=True, check=True)
 subprocess.run(["cmake --build build --target VisualT_doxygen_generate_xml"], cwd="../..", shell=True, check=True)
 with open("../../build/doc/conf.py") as infile:
