39c0626e554f9d0b3b272950f776769c77d47e31
Nemo-LUCE <artemiscode@hotmail.it>
2021-02-01 17:51:55 +0100

    readthedocs support
    attempt 17
    - copy workaround with cmake
    
    #no-ci

--- doc/readthedocs/conf.py
+++ doc/readthedocs/conf.py
@@ -1,9 +1,8 @@
 # -- readthedocs CI ----------------------------------------------------------
 
 import subprocess
-from pyrocopy import pyrocopy
 
-pyrocopy.copy("..", ".", excludeDirs=["readthedocs"])
+subprocess.run(["cmake -P copy.cmake"], shell=True, check=True)
 subprocess.run(["cmake -D VisualT_ASSUME_LITTLE_ENDIAN:BOOL=YES -S . -B build"], cwd="../..", shell=True, check=True)
 subprocess.run(["cmake --build build --target VisualT_doxygen_generate_xml"], cwd="../..", shell=True, check=True)
 with open("../../build/doc/conf.py") as infile:
