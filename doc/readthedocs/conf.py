d025fcea75e472132c2a15a89807c290e389e81c
Nemo-LUCE <artemiscode@hotmail.it>
2021-02-02 12:04:43 +0100

    fixed readthedocs cmake script #3
    #no-build

--- doc/readthedocs/conf.py
+++ doc/readthedocs/conf.py
@@ -4,6 +4,6 @@ import subprocess
 import os
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
-subprocess.run(["cmake", "-P", "ci.cmake"], cwd=current_dir, check=True)
+subprocess.run(["cmake -P ci.cmake"], cwd=current_dir, shell=True, check=True)
 with open("../../build/doc/conf.py") as infile:
     exec(infile.read())
