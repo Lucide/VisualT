16c99c7f7cb92475bfb0e5258c24860fa40a25b5
Nemo-LUCE <artemiscode@hotmail.it>
2021-02-02 12:10:18 +0100

    fixed readthedocs cmake script #4
    #no-build

--- doc/readthedocs/ci.cmake
+++ doc/readthedocs/ci.cmake
@@ -1,10 +1,11 @@
 set(cwd "${CMAKE_CURRENT_LIST_DIR}/../..")
 
-file(REMOVE_RECURSE "${cwd}/build")
 file(COPY "${cwd}/doc/" DESTINATION ${CMAKE_CURRENT_LIST_DIR}
      PATTERN "readthedocs" EXCLUDE)
 
 execute_process(COMMAND ${CMAKE_COMMAND} -D VisualT_ASSUME_LITTLE_ENDIAN:BOOL=YES -S . -B build
-                COMMAND ${CMAKE_COMMAND} --build build --target VisualT_doxygen_generate_xml
+                WORKING_DIRECTORY ${cwd}
+                COMMAND_ECHO STDOUT)
+execute_process(COMMAND ${CMAKE_COMMAND} --build build --target VisualT_doxygen_generate_xml
                 WORKING_DIRECTORY ${cwd}
                 COMMAND_ECHO STDOUT)
